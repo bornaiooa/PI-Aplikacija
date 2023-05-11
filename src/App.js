a39833a7ba0b2593d163bd0d138b58c7203bbb9b
ivanagrbac <ivana.grbac3@gmail.com>
2023-05-11 12:04:33 +0200

    Mapiranje

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-//import logo from './logo.svg';
-import './App.css';
-import { Login } from "./Login";
-import { Registracija } from "./Registracija";
+import './frontend/css/App.css';
+import { Login } from "./frontend/components/Auth/Login";
+import { Registracija } from "./frontend/components/Auth/Registracija";
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
